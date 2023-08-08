ee1c42b57a984027e786c7fa45f1be87174baf1b
irfanyazid-13032000 <irfanyazid18@gmail.com>
2023-08-08 16:03:34 +0700

    membuat belajar.blade.php di folder livewire

---
+++ app/Http/Livewire/Belajar.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Belajar extends Component
+{
+    public $nama = 'yajed';
+    public function render()
+    {
+        return view('livewire.belajar');
+    }
+}
